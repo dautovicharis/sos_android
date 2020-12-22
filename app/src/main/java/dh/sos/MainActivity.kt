7f6547e688f14fa529cc0c6afd22dc1f5f605b0c
Haris Dautovic <haris.dautovic.dev@gmail.com>
2020-12-22 10:52:58 +0100

    Fix for q_65346039

--- app/src/main/java/dh/sos/MainActivity.kt
+++ app/src/main/java/dh/sos/MainActivity.kt
@@ -1,11 +1,49 @@
 package dh.sos
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.idenfy.permissionissue.R
 
 class MainActivity : AppCompatActivity() {
+    var permissionsDelegateUtil = PermissionsDelegateUtil()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(PermissionsDelegateUtil.RESULT_TAG, "onCreate")
+        if (!permissionsDelegateUtil.hasCameraPermission(this))
+            permissionsDelegateUtil.requestCameraPermission(this)
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val result = permissionsDelegateUtil.resultGranted(this, requestCode, permissions, grantResults)
+        Log.d(PermissionsDelegateUtil.RESULT_TAG, "onRequestPermissionsResult:$result")
+        handleCameraPermissionResult(permissionResult = result)
+    }
+
+    private fun handleCameraPermissionResult(permissionResult: PermissionResult) {
+        when (permissionResult) {
+            PermissionResult.PermissionGranted -> {
+
+            }
+            PermissionResult.PermissionNotGrantedRetryAuto -> {
+                permissionsDelegateUtil.requestCameraPermission(this)
+
+            }
+            PermissionResult.PermissionNotGrantedCantRetry -> {
+                permissionsDelegateUtil.requestCameraPermission(this)
+            }
+            PermissionResult.PermissionNotGrantedDontAsk -> {
+            }
+        }
     }
 }
---
+++ app/src/main/java/dh/sos/PermissionDelegateUtil.kt
@@ -0,0 +1,75 @@
+package dh.sos
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.annotation.Keep
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+
+class PermissionsDelegateUtil {
+    fun hasCameraPermission(fragment: FragmentActivity): Boolean {
+        return ContextCompat.checkSelfPermission(
+                fragment,
+                Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun requestCameraPermission(fragment: FragmentActivity) {
+        ActivityCompat.requestPermissions(
+                fragment,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_PERMISSIONS_CAMERA
+        )
+    }
+
+    fun resultGranted(
+            fragment: FragmentActivity, requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ): PermissionResult {
+        if (requestCode != REQUEST_PERMISSIONS_CAMERA) {
+            Log.d(RESULT_TAG, "requestCode does not match")
+            return PermissionResult.PermissionNotGrantedDontAsk
+        }
+        if (grantResults.isEmpty()) {
+            Log.d(RESULT_TAG, "grantResults.isEmpty()")
+            return PermissionResult.PermissionNotGrantedDontAsk
+        }
+        if (permissions[0] != Manifest.permission.CAMERA) {
+            Log.d(RESULT_TAG, "not camera")
+            return PermissionResult.PermissionNotGrantedDontAsk
+        }
+        return if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            PermissionResult.PermissionGranted
+
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (fragment.shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                    PermissionResult.PermissionNotGrantedRetryAuto
+                } else {
+                    ///User selected don't show again checkbox.
+                    PermissionResult.PermissionNotGrantedCantRetry
+                }
+            } else {
+                PermissionResult.PermissionNotGrantedRetryAuto
+            }
+        }
+    }
+
+    @Keep
+    companion object {
+        const val RESULT_TAG = "CAMERA_PERMISSION"
+        const val REQUEST_PERMISSIONS_CAMERA = 1001
+    }
+
+}
+
+sealed class PermissionResult {
+    object PermissionGranted : PermissionResult()
+    object PermissionNotGrantedRetryAuto : PermissionResult()
+    object PermissionNotGrantedDontAsk : PermissionResult()
+    object PermissionNotGrantedCantRetry : PermissionResult()
+}
