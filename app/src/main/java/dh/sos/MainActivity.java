429bda20450e921c6631220c15c629d345d9d87d
Haris Dautovic <haris.dautovic.dev@gmail.com>
2021-07-18 09:37:53 +0200

    Solution for q_68375499 - v3

--- app/src/main/java/dh/sos/MainActivity.java
+++ app/src/main/java/dh/sos/MainActivity.java
@@ -1,28 +1,29 @@
 package dh.sos;
 
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView imageView = findViewById(R.id.iv);
+        final ImageView imageView2 = findViewById(R.id.imgView2);
         final SeekBar seekbar = findViewById(R.id.simpleSeekBar);
 
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                addOverlay(R.drawable.overlay, imageView, imageView.getWidth() * progress / 100);
+                imageView2.setImageBitmap(getPart(imageView2, progress, R.drawable.nature2));
             }
 
             @Override
@@ -34,14 +35,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Initial overlay
-        imageView.post(() -> addOverlay(R.drawable.overlay, imageView, 0));
+
+        imageView2.post(new Runnable() {
+            @Override
+            public void run() {
+                imageView2.setImageBitmap(getPart(imageView2, 50, R.drawable.nature2));
+            }
+        });
     }
 
-    private void addOverlay(int resourceId, ImageView imageView, int startMargin) {
-        imageView.getOverlay().clear();
-        Drawable drawable = ResourcesCompat.getDrawable(getResources(), resourceId, null);
-        drawable.setBounds(new Rect(startMargin, 0, imageView.getWidth(), imageView.getHeight()));
-        imageView.getOverlay().add(drawable);
+    private Bitmap getPart(ImageView imageView, int progress, int resource) {
+        imageView.invalidate();
+        BitmapDrawable drawable = (BitmapDrawable) getResources().getDrawable(resource);
+        Bitmap bitmap = drawable.getBitmap();
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = displayMetrics.widthPixels;
+
+        int dynamicWidth = (int) (width * progress) / 100;
+        if (dynamicWidth > 0) {
+            return Bitmap.createBitmap(bitmap, 0, 0, dynamicWidth, bitmap.getHeight());
+        }
+        return null;
     }
 }
